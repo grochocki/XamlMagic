6e48ee1e1ed6c086caae60986aace1fbf5f3adf8
Keith Harvey <keith.harvey@intergraph.com>
2016-02-01 11:12:42 -0600

    Added test for attached property syntax in an attribute.

---
+++ UnitTests/UnitTests.AttachedProperty.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using XamlMagic.Service.Options;
+
+namespace XamlMagic.UnitTests
+{
+    [TestFixture]
+    public sealed partial class UnitTests
+    {
+        [Test]
+        public void TestAttachedProperty()
+        {
+            var stylerOptions = new StylerOptions(config: this.defaultConfig)
+			{
+				IndentWithTabs = true
+			};
+
+            this.DoTest(stylerOptions);
+        }
+    }
+}
